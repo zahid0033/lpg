75943b5c59ce6c1ab820ca8c8b032c987db5ad8c
rifatron999 <rifatron999@gmail.com>
2019-11-17 15:11:10 +0600

    Rifat 0.1

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -105,7 +105,7 @@
                                     </div>
                                     <div class="col-md-8">
                                         <p>The principles and standards of Dream LPG BD. has made the company one of the most reputed trading organization in Bangladesh and we are
-                                            able to satisfy all our clients need.</p>
+                                            able to satisfy all our clients need. RIFAT</p>
                                     </div>
                                 </div>
                             </div>
